3a9f0043dfe895dd52069d1d8e735b7c3a321c34
Marlon Matthew <mbm.drums@gmail.com>
2021-04-20 13:13:36 -0700

    Added db class to index.js in db

---
+++ db/index.js
@@ -0,0 +1,44 @@
+const connection = require("./connection");
+
+class DB {
+  constructor(connection) {
+    this.connection = connection;
+  }
+
+  getEmployees() {
+    return this.connection.query(
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary"
+    );
+  }
+
+  createEmployee(employee) {
+    return this.connection.query("INSERT INTO employee SET ?", employee);
+  }
+
+  updateEmployeeRole(employeeId, roleId) {
+    return this.connection.query(
+      "UPDATE employee SET role_id = ? WHERE id = ?",
+      [roleId, employeeId]
+    );
+  }
+
+  getRoles() {
+    return this.connection.query(
+      "SELECT role.id, role.title, department.name AS department"
+    );
+  }
+
+  createRole(role) {
+    return this.connection.query("INSERT INTO role SET ?", role);
+  }
+
+  getDepartments() {
+    return this.connection.query("SELECT department.id, department.name");
+  }
+
+  createDepartment(department) {
+    return this.connection.query("INSERT INTO department SET ?", department);
+  }
+}
+
+module.exports = new DB(connection);
